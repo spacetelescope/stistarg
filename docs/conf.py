cf4338c44d09e170ed3befac5304a588a44c12ae
Sean Lockwood <lockwood@stsci.edu>
2017-08-09 19:00:49 -0400

    [skip ci] Attempting to fix "page source" link at bottom of readthedocs version of documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.txt']
 
 # The master toctree document.
 master_doc = 'index'
@@ -116,7 +116,7 @@ html_sidebars = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------

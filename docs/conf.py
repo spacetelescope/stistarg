c50d12ec984eadfd294b6892b219228388966aa5
Sean Lockwood <lockwood@stsci.edu>
2018-07-27 16:03:28 -0400

    [skip ci] Attempting to get mock working.

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['np', 'astropy.io']
+MOCK_MODULES = ['numpy', ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------

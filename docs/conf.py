1df350c39284b8f9bab0d95e3138586769b2ca9b
Sean Lockwood <lockwood@stsci.edu>
2018-07-27 16:12:38 -0400

    [skip ci] reducing RTD dependencies to fit within Docker size constraints.

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', ]
+MOCK_MODULES = ['numpy', 'matplotlib', 'astropy', 'scipy', 'stistools', ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
